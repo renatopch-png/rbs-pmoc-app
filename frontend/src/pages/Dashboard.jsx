9449b6042dcb1d750b2c5495fd894a3332b19cce
renatopch-png <renato.pch@gmail.com>
2026-08-25 21:30:48 -0300

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { collection, getDocs, query, orderBy } from "firebase/firestore";
-import { db } from "../../services/firebase"; 
+import { db } from "../services/firebase"; 
 import { NotificacoesBanner } from "../../components/Notificacoes/NotificacoesManuencao";
 
 export default function Dashboard() {
