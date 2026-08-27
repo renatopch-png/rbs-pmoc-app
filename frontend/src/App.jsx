2cab22b7cd397ae62de5b29a81fe8a1d8161ad7b
renatopch-png <renato.pch@gmail.com>
2026-08-26 22:45:10 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import Cronograma from "./pages/Cronograma";
 import ListaOS from "./pages/OrdemServico/ListaOS";
 import ExecucaoOS from "./pages/OrdemServico/ExecucaoOS";
 import GeradorPMOC from "./pages/Relatorios/GeradorPMOC";
+import GeradorPMOCConsolidado from "./pages/Relatorios/GeradorPMOCConsolidado";
 import RelatorioPublico from "./pages/Relatorios/RelatorioPublico";
 import GeradorART from "./pages/Relatorios/GeradorART";
 import GeradorContrato from "./pages/Relatorios/GeradorContrato";
@@ -38,6 +39,7 @@ export default function App() {
         <Route path="/ordens-servico" element={<Privada><ListaOS /></Privada>} />
         <Route path="/ordens-servico/executar/:equipamentoId" element={<Privada><ExecucaoOS /></Privada>} />
         <Route path="/relatorios/pmoc/:osId" element={<Privada><GeradorPMOC /></Privada>} />
+        <Route path="/relatorios/pmoc-consolidado/:clienteId" element={<Privada><GeradorPMOCConsolidado /></Privada>} />
         <Route path="/relatorios/art/:osId" element={<Privada><GeradorART /></Privada>} />
         <Route path="/relatorios/contrato/:clienteId" element={<Privada><GeradorContrato /></Privada>} />
         <Route path="/relatorios" element={<Privada><Relatorios /></Privada>} />
